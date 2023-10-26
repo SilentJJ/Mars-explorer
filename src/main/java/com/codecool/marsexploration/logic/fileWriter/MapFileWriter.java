61ece036c118f245dde1f4908b70e266f3783d53
PeterH <hegyespeter05@gmail.com>
2023-10-26 10:59:55 +0200

    slightly clean code refactor file writer

--- src/main/java/com/codecool/marsexploration/logic/fileWriter/MapFileWriter.java
+++ src/main/java/com/codecool/marsexploration/logic/fileWriter/MapFileWriter.java
@@ -12,21 +12,8 @@ public class MapFileWriter implements FileWriter{
         this.map = map;
     }
 
-    private static String readFromInputStream(InputStream inputStream)
-            throws IOException {
-        StringBuilder resultStringBuilder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                resultStringBuilder.append(line).append("\n");
-            }
-        }
-        return resultStringBuilder.toString();
-    }
-
     @Override
     public void writeMapToFile() {
-        //String oneLine = String.join("", String.copyValueOf(map.getMap()[0]));
         String mapInOne = "";
         for (char[] line : map.getMap()) {
             mapInOne += String.join("", String.copyValueOf(line)) + "\n";

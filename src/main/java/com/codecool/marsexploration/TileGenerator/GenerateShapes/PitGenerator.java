161424ee44bc007d9e01f5e55fff8dd228709867
SilentJJ <testy@freemail.hu>
2023-10-26 10:33:59 +0200

    add tile validator to the generators

--- src/main/java/com/codecool/marsexploration/TileGenerator/GenerateShapes/PitGenerator.java
+++ src/main/java/com/codecool/marsexploration/TileGenerator/GenerateShapes/PitGenerator.java
@@ -2,9 +2,10 @@ package com.codecool.marsexploration.TileGenerator.GenerateShapes;
 
 import com.codecool.marsexploration.TileGenerator.GenerateShapes.ShapeGenerator;
 import com.codecool.marsexploration.TileGenerator.TileEnum;
+import com.codecool.marsexploration.TileGenerator.TileValidator;
 
 public class PitGenerator extends ShapeGenerator {
-    public PitGenerator() {
-        super(TileEnum.PIT.getTileChar());
+    public PitGenerator(TileValidator validator) {
+        super(TileEnum.PIT.getTileChar(), validator);
     }
 }
